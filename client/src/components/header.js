570de05cde90f1a27b3e1f2f3931e8d09035b849
sam-schmitt <samuelgschmitt@gmail.com>
2021-08-05 14:54:27 -0400

    header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,9 +1,33 @@
 import React from "react";
 import logo from "../assets/img/logo.png";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router";
+
 export default function Header() {
+	const history = useHistory();
+
 	return (
 		<div className='header'>
 			<img src={logo} alt={"logo"} />
+			<Button
+				color='white'
+				onClick={() => {
+					history.push("/finished");
+				}}
+				style={{ flex: 1 }}
+			>
+				<p className='headerButton'>Finished Items</p>
+			</Button>
+			<Button
+				className='headerButton'
+				color='white'
+				onClick={() => {
+					history.push("/");
+				}}
+				style={{ flex: 1 }}
+			>
+				<p className='headerButton'>Todo Items</p>
+			</Button>
 		</div>
 	);
 }
