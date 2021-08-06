4e03f60f291b101d05cbafb33f4797128812322c
mypoliticsweb <73365506+QuaeDev@users.noreply.github.com>
2021-08-06 18:59:31 -0400

    added navbar buttons

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -2,32 +2,44 @@ import React from "react";
 import logo from "../assets/img/logo.png";
 import Button from "@material-ui/core/Button";
 import { useHistory } from "react-router";
+import { withStyles } from "@material-ui/core/styles";
+import { brown, green, red } from "@material-ui/core/colors";
 
 export default function Header() {
 	const history = useHistory();
-
+	const HeaderButton = withStyles((theme) => ({
+		root: {
+			color: "#fff",
+			backgroundColor: brown[900],
+			marginLeft: 3,
+			marginRight: 3,
+			height: "100%",
+			flex: 0.25,
+			"&:hover": {
+				backgroundColor: brown[600],
+				color: "#fff",
+			},
+		},
+	}))(Button);
 	return (
-		<div className='header'>
-			<img src={logo} alt={"logo"} />
-			<Button
-				color='white'
-				onClick={() => {
-					history.push("/finished");
-				}}
-				style={{ flex: 1 }}
-			>
-				<p className='headerButton'>Finished Items</p>
-			</Button>
-			<Button
-				className='headerButton'
-				color='white'
+		<div className="header">
+			<img src={logo} alt={"logo"} className="header-logo" />
+			<HeaderButton
+				color="white"
 				onClick={() => {
 					history.push("/");
 				}}
-				style={{ flex: 1 }}
 			>
-				<p className='headerButton'>Todo Items</p>
-			</Button>
+				<p className="headerButton">Todo Items</p>
+			</HeaderButton>
+			<HeaderButton
+				color="white"
+				onClick={() => {
+					history.push("/finished");
+				}}
+			>
+				<p className="headerButton">Finished Items</p>
+			</HeaderButton>
 		</div>
 	);
 }
